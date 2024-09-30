7abb1771f9279ea4265ce636f31ace5aaa0af7d5
Radu Marginean <radu.marg@gmail.com>
2024-09-30 14:37:51 +0300

    Correct developemen status.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
       ],
   classifiers=[
     # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" 
-    'Development Status :: 4 - Alpha',
+    'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
